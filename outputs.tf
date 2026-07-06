5176d31a1ef8c6f921d38a2d5b7f2827081b3524
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:13:57 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,12 @@ output "nat_gateway_public_ip_associations" {
   description = "All nat_gateway_public_ip_association resources"
   value       = azurerm_nat_gateway_public_ip_association.nat_gateway_public_ip_associations
 }
+output "nat_gateway_public_ip_associations_nat_gateway_id" {
+  description = "List of nat_gateway_id values across all nat_gateway_public_ip_associations"
+  value       = [for k, v in azurerm_nat_gateway_public_ip_association.nat_gateway_public_ip_associations : v.nat_gateway_id]
+}
+output "nat_gateway_public_ip_associations_public_ip_address_id" {
+  description = "List of public_ip_address_id values across all nat_gateway_public_ip_associations"
+  value       = [for k, v in azurerm_nat_gateway_public_ip_association.nat_gateway_public_ip_associations : v.public_ip_address_id]
+}
 
